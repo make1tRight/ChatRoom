e9d54c8138e64d4ffbb959af0af436b9ebdc5a30
make1tRight <xliang9809@163.com>
2025-03-01 11:34:50 +0800

    update

---
+++ GateServer/build/CMakeFiles/GateServer.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/admin/workspace/InstantMessaging/GateServer/AsioIOContextPool.cpp" "CMakeFiles/GateServer.dir/AsioIOContextPool.cpp.o" "gcc" "CMakeFiles/GateServer.dir/AsioIOContextPool.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/CServer.cpp" "CMakeFiles/GateServer.dir/CServer.cpp.o" "gcc" "CMakeFiles/GateServer.dir/CServer.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/ConfigMgr.cpp" "CMakeFiles/GateServer.dir/ConfigMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/ConfigMgr.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/GateServer.cpp" "CMakeFiles/GateServer.dir/GateServer.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/HttpConnection.cpp" "CMakeFiles/GateServer.dir/HttpConnection.cpp.o" "gcc" "CMakeFiles/GateServer.dir/HttpConnection.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/LogicSystem.cpp" "CMakeFiles/GateServer.dir/LogicSystem.cpp.o" "gcc" "CMakeFiles/GateServer.dir/LogicSystem.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/MysqlDao.cpp" "CMakeFiles/GateServer.dir/MysqlDao.cpp.o" "gcc" "CMakeFiles/GateServer.dir/MysqlDao.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/MysqlMgr.cpp" "CMakeFiles/GateServer.dir/MysqlMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/MysqlMgr.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/RedisMgr.cpp" "CMakeFiles/GateServer.dir/RedisMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/RedisMgr.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/StatusGrpcClient.cpp" "CMakeFiles/GateServer.dir/StatusGrpcClient.cpp.o" "gcc" "CMakeFiles/GateServer.dir/StatusGrpcClient.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/VarifyGrpcClient.cpp" "CMakeFiles/GateServer.dir/VarifyGrpcClient.cpp.o" "gcc" "CMakeFiles/GateServer.dir/VarifyGrpcClient.cpp.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/message.grpc.pb.cc" "CMakeFiles/GateServer.dir/message.grpc.pb.cc.o" "gcc" "CMakeFiles/GateServer.dir/message.grpc.pb.cc.o.d"
+  "/home/admin/workspace/InstantMessaging/GateServer/message.pb.cc" "CMakeFiles/GateServer.dir/message.pb.cc.o" "gcc" "CMakeFiles/GateServer.dir/message.pb.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
