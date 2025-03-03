4777a73be819b2eeb25a9b2d618e3f86537900e1
make1tRight <xliang9809@163.com>
2025-03-03 22:15:55 +0800

    update cmake

---
+++ build/CMakeFiles/GateServer.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tom/workspace/ChatRoom/GateServer/AsioIOContextPool.cpp" "CMakeFiles/GateServer.dir/GateServer/AsioIOContextPool.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/AsioIOContextPool.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/CServer.cpp" "CMakeFiles/GateServer.dir/GateServer/CServer.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/CServer.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/ConfigMgr.cpp" "CMakeFiles/GateServer.dir/GateServer/ConfigMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/ConfigMgr.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/GateServer.cpp" "CMakeFiles/GateServer.dir/GateServer/GateServer.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/GateServer.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/HttpConnection.cpp" "CMakeFiles/GateServer.dir/GateServer/HttpConnection.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/HttpConnection.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/LogicSystem.cpp" "CMakeFiles/GateServer.dir/GateServer/LogicSystem.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/LogicSystem.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/MysqlDao.cpp" "CMakeFiles/GateServer.dir/GateServer/MysqlDao.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/MysqlDao.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/MysqlMgr.cpp" "CMakeFiles/GateServer.dir/GateServer/MysqlMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/MysqlMgr.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/RedisMgr.cpp" "CMakeFiles/GateServer.dir/GateServer/RedisMgr.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/RedisMgr.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/StatusGrpcClient.cpp" "CMakeFiles/GateServer.dir/GateServer/StatusGrpcClient.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/StatusGrpcClient.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/VarifyGrpcClient.cpp" "CMakeFiles/GateServer.dir/GateServer/VarifyGrpcClient.cpp.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/VarifyGrpcClient.cpp.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/message.grpc.pb.cc" "CMakeFiles/GateServer.dir/GateServer/message.grpc.pb.cc.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/message.grpc.pb.cc.o.d"
+  "/home/tom/workspace/ChatRoom/GateServer/message.pb.cc" "CMakeFiles/GateServer.dir/GateServer/message.pb.cc.o" "gcc" "CMakeFiles/GateServer.dir/GateServer/message.pb.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GateServer.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GateServer.dir/GateServer/AsioIOContextPool.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/AsioIOContextPool.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/CServer.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/CServer.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/ConfigMgr.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/ConfigMgr.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/GateServer.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/GateServer.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/HttpConnection.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/HttpConnection.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/LogicSystem.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/LogicSystem.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/MysqlDao.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/MysqlDao.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/MysqlMgr.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/MysqlMgr.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/RedisMgr.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/RedisMgr.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/StatusGrpcClient.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/StatusGrpcClient.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/VarifyGrpcClient.cpp.o"
+  "CMakeFiles/GateServer.dir/GateServer/VarifyGrpcClient.cpp.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/message.grpc.pb.cc.o"
+  "CMakeFiles/GateServer.dir/GateServer/message.grpc.pb.cc.o.d"
+  "CMakeFiles/GateServer.dir/GateServer/message.pb.cc.o"
+  "CMakeFiles/GateServer.dir/GateServer/message.pb.cc.o.d"
+  "GateServer"
+  "GateServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GateServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
